0acd8f225e14347609f168ca70dc931e233f21c6
leonardo <l.castillo1989@Hotmail.com>
2018-06-01 12:16:57 -0400

    fixing ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -827,20 +827,6 @@ $( '.form-ajax' ).on( 'keyup', 'input.validate-locally', function() {
 });
 
 //	AJAX call
-$( '.form-ajax' ).submit(function(e) {
-	e.preventDefault();
-	var $this = $( this ),
-			action = $this.attr( 'action' );
-
-	// The AJAX requrest
-	$.post(
-			action,
-			$this.serialize(),
-			function( data ) {
-				$( '.ajax-message' ).html( data );
-			}
-	);
-});
 
 //	Validates the fileds
 function validateField ( field ) {
